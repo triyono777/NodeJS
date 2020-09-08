65e5f7f021728526e6e6873cdae97206026066f8
triyono <yono.tri@gmail.com>
2020-09-09 06:02:15 +0700

    set response

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require('http');
+const { title } = require('process');
 
 const server =  http.createServer((req,res)=>{
     console.log(req.url,req.method,req.headers);
     // process.exit();
+    res.setHeader('Content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>first node</title></head>');
+    res.write('<body><h1>hello from node.js</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
