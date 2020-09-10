c798da9b81bba611dbed960ebc588567e0abe3bd
triyono777 <yono.tri>
2020-09-10 21:52:05 +0700

    change to async

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
 const http = require('http');
-const { title } = require('process');
+const {
+    title
+} = require('process');
 const fs = require('fs');
 
-const server =  http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
     // process.exit();
 
-    if(req.url==='/'){
+    if (req.url === '/') {
         res.write('<html>');
         res.write('<head><title>enter message</title></head>');
-       res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
-       return res.end();
+        return res.end();
     }
 
-if(req.url==='/message'&& req.method==='POST'){
-    const body = [];
-    req.on('data',(chunks)=>{
-        console.log(chunks);
-        body.push(chunks);
+    if (req.url === '/message' && req.method === 'POST') {
+        const body = [];
+        req.on('data', (chunks) => {
+            console.log(chunks);
+            body.push(chunks);
 
-    });
-    req.on('end',()=>{
-        const parseBody = Buffer.concat(body).toString();
-       const message = parseBody.split('=')[1];
-       fs.writeFileSync('message.txt',message);
-    });
-    res.statusCode == 302;
-    res.setHeader('Location','/');
-   return res.end();
+        });
+        return req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode == 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
 
-}
-  
 
-    res.setHeader('Content-type','text/html');
+    }
+
+
+    res.setHeader('Content-type', 'text/html');
     res.write('<html>');
     res.write('<head><title>first node</title></head>');
     res.write('<body><h1>hello from node.js</h1></body>');
@@ -47,7 +51,7 @@ server.listen(3000);
 
 //// named function
 // function rqListener (req,res) {
-    
+
 // }
 // http.createServer(rqListener);
 
@@ -60,4 +64,3 @@ server.listen(3000);
 // http.createServer((req,res)=>{
 //     console.log(req);
 // });
-
