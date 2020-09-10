722f6f61442c7ed438b2a9727c766babb6a73848
triyono777 <yono.tri>
2020-09-10 21:32:22 +0700

    parsing request bodies

--- app.js
+++ app.js
@@ -15,7 +15,17 @@ const server =  http.createServer((req,res)=>{
     }
 
 if(req.url==='/message'&& req.method==='POST'){
-    fs.writeFileSync('message.txt','DUMMY');
+    const body = [];
+    req.on('data',(chunks)=>{
+        console.log(chunks);
+        body.push(chunks);
+
+    });
+    req.on('end',()=>{
+        const parseBody = Buffer.concat(body).toString();
+       const message = parseBody.split('=')[1];
+       fs.writeFileSync('message.txt',message);
+    });
     res.statusCode == 302;
     res.setHeader('Location','/');
    return res.end();
